0b8bc02344527abb1564db2038a87aaf9317fbcf
krokib <bblondeau@netcourrier.com>
2025-02-02 04:17:04 +0100

    Ajout de Terre15.java

---
+++ Terre15.java
@@ -0,0 +1,18 @@
+/*
+ * Epreuve terre - Terre : ok
+ * 
+ * Créez un programme qui célèbre votre victoire.
+ * 
+ * Exemples d’utilisation :
+ * $> ruby exo.rb
+ * J’ai terminé l’Épreuve de la Terre et c’était [].
+ * 
+ * Note : [] est à remplacer par un adjectif de votre choix (facile ?)
+ */
+
+ public class Terre15 {
+ 
+    public static void main(String[] args) {
+        System.out.println("J’ai terminé l’Épreuve de la Terre et c’était une bonne remise en jambe !");
+    }
+ }

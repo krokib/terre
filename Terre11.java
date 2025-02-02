c11b3e955c931dc60f4779f3ec20cbe5de87c39b
krokib <bblondeau@netcourrier.com>
2025-02-02 02:41:52 +0100

    Refactoring de la récupération et la conversion du nombre d'heures

--- Terre11.java
+++ Terre11.java
@@ -25,7 +25,6 @@
     private static void changeFormatHeure(String str) {
         final char[] HEURE24 = str.toCharArray();
         char[] heure12 = new char[5];
-        boolean morning = true;
         
         //indice de ':' pour traiter H:MM et HH:MM séparément
         int indexDoubleCot = 0;
@@ -33,48 +32,33 @@
             indexDoubleCot++;
         }
         
-        //Remplissage de heure12 ':MM'
+        //Remplissage des minutes de heure12 ':MM'
         for (int i = 0; i < 3; i++) {
             heure12[2 + i] = HEURE24[indexDoubleCot + i];
         }
         
-        //Récupération des heures de HEURE24 dans un char[2]
-        char[] heures24CHAR = new char[2];
-        if (indexDoubleCot == 1) {
-            heures24CHAR[0] = '0';
-            heures24CHAR[1] = HEURE24[0];
-        } else {
-            for (int i = 0; i < 2; i++) {
-                heures24CHAR[i] = HEURE24[i];
-            }
+        //Récupération du nombre d'heures de HEURE24 dans un char[]
+        char[] heures24CHAR = new char[indexDoubleCot];
+        for (int i = 0; i < indexDoubleCot; i++) {
+            heures24CHAR[i] = HEURE24[i];
         }
 
-        //conversion des heures de HEURE24 en int
+        //Conversion des heures de HEURE24 en int
         int heures24INT = Integer.parseInt(String.copyValueOf(heures24CHAR));
 
-        //Remplissage de heure12 'HH'
-        if (heures24INT == 0) {
-            //HEURE24 entre 00:00 et 00:59
-            morning = true;
-            heure12[0] = '1';
-            heure12[1] = '2';
-        } else if (heures24INT > 0 && heures24INT < 12) {
-            //HEURE24 entre 1:00 et 11:59
-            morning = true;
-            heure12[0] = heures24CHAR[0];
-            heure12[1] = heures24CHAR[1];
-        } else if (heures24INT == 12) {
-            //HEURE24 entre 12:00 et 12:59
-            morning = false;
-            heure12[0] = '1';
-            heure12[1] = '2';
+        //Conversion des heures 24 en 12
+        int heures12INT;
+        boolean morning;
+        if (heures24INT == 0 || heures24INT == 12) {
+            morning = (heures24INT == 0);
+            heures12INT = 12;
         } else {
-            //HEURE24 entre 13:00 et 23:59
-            morning = false;
-            int heures12INT = heures24INT - 12;
-            heure12[0] = Character.forDigit((heures12INT / 10), 10);
-            heure12[1] = Character.forDigit((heures12INT % 10), 10);
+            morning = (heures24INT < 12);
+            heures12INT = morning ? heures24INT : heures24INT - 12;
         }
+
+        heure12[0] = Character.forDigit((heures12INT / 10), 10);
+        heure12[1] = Character.forDigit((heures12INT % 10), 10);
 
         System.out.println(String.copyValueOf(heure12) + (morning ? "AM" : "PM"));
     }
